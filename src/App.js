bee18b0be19742405a98d3d6ae01f46f8650b5b6
Vladimir Minin <v_minin@rambler.ru>
2023-02-02 12:00:52 +0300

    first component

--- src/App.js
+++ src/App.js
@@ -1,7 +1,12 @@
+import CostItem from "./components/CostItem";
+
 function App() {
   return (
     <div>
       <h1>Начнем изучение React!</h1>
+      <h2>Hey there</h2>
+      <p>I'm here</p>
+      <CostItem></CostItem>
     </div>
   );
 }
---
+++ src/components/CostItem.js
@@ -0,0 +1,13 @@
+function CostItem() {
+  return (
+    <div>
+      <div>март 21 2021</div>
+      <div>
+        <h2>Холодильник</h2>
+        <div>$999.99</div>
+      </div>
+    </div> 
+  );
+};
+
+export default CostItem;
