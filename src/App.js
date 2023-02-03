01edc2b2985b7926c69485d29cbe5fa90a90f3e6
Vladimir Minin <v_minin@rambler.ru>
2023-02-03 09:35:13 +0300

    dynamic data

--- src/App.js
+++ src/App.js
@@ -4,8 +4,6 @@ function App() {
   return (
     <div>
       <h1>Начнем изучение React!</h1>
-      <h2>Hey there</h2>
-      <p>I'm here</p>
       <CostItem></CostItem>
     </div>
   );
--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,10 +1,17 @@
+import './CostItem.css';
+
 function CostItem() {
+
+  const costDate = new Date(2021, 2, 21);
+  const costDescription = 'Холодильник';
+  const costAmount = 999.99
+
   return (
-    <div>
-      <div>март 21 2021</div>
-      <div>
-        <h2>Холодильник</h2>
-        <div>$999.99</div>
+    <div className='cost-item'>
+      <div>{costDate.toISOString()}</div>
+      <div className='cost-item__description'>
+        <h2>{costDescription}</h2>
+        <div className='cost-item__price'>${costAmount}</div>
       </div>
     </div> 
   );
