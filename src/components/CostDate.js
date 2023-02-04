68c9827e8a38964c9b4ab9e12bed2e365d871e7c
Vladimir Minin <v_minin@rambler.ru>
2023-02-04 22:42:02 +0300

    costdate

--- src/components/CostDate.js
+++ src/components/CostDate.js
@@ -1,13 +1,15 @@
+import './CostDate.css'
+
 function CostDate(props) {
   const month = props.date.toLocaleString('ru-RU', {month: "long"});
   const day = props.date.toLocaleString('ru-RU', {day: "2-digit"});
   const year = props.date.getFullYear();
 
   return (
-    <div>
-        <div>{month}</div>
-        <div>{day}</div>
-        <div>{year}</div>
+    <div className='cost-date'>
+        <div className='cost-date__month'>{month}</div>
+        <div className='cost-date__day'>{day}</div>
+        <div className='cost-date__year'>{year}</div>
       </div>
   )
 }
