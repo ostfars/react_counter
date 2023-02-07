7fb53f6079cb0994193936ccf828ac772edd1a8d
ostfars <v_minin@rambler.ru>
2023-02-07 22:26:49 +0300

    monday

--- src/components/Costs/CostItem.js
+++ src/components/Costs/CostItem.js
@@ -1,16 +1,26 @@
 import './CostItem.css';
 import CostDate from './CostDate';
 import Card from '../UI/Card';
+import React, { useState } from 'react';
 
 function CostItem(props) {
+
+useState();
+
+let description = props.description;
+
+const changeDescription = () => {
+
+}
 
   return (
     <Card className='cost-item'>
       <CostDate date={props.date}/>
       <div className='cost-item__description'>
-        <h2>{props.description}</h2>
+        <h2>{description}</h2>
         <div className='cost-item__price'>${props.amount}</div>
       </div>
+      <button onClick={changeDescription}>Изменить описание</button>
     </Card> 
   );
 };
