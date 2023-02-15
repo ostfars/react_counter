1cce05b63135f62d72b1e8a372af0d470651aea5
ostfars <v_minin@rambler.ru>
2023-02-15 21:27:04 +0300

    monday

--- src/App.js
+++ src/App.js
@@ -5,25 +5,33 @@ function App() {
 
   const costs = [
     {
+      id: 'c1',
       date: new Date(2021, 2, 21),
       description: 'Холодильник',
       amount: 999.99
     },
     {
+      id: 'c2',
       date: new Date(2021, 11, 25),
       description: 'MacBook',
       amount: 1254.72
     },
     {
+      id: 'c3',
       date: new Date(2021, 3, 1),
       description: 'Джинсы',
       amount: 49.99
     }
   ];
 
+  const addCostHandler = (cost) => {
+    console.log(cost);
+    console.log('APP');
+  }
+
   return (
     <div>
-      <NewCost />
+      <NewCost onAddCost={addCostHandler} />
       <Costs {...costs}/>  
     </div>
   );
--- src/components/NewCost/CostForm.js
+++ src/components/NewCost/CostForm.js
@@ -1,37 +1,79 @@
 import React, {useState} from 'react';
 import './CostForm.css'
 
-const CostForm = () => {
+const CostForm = (props) => {
 
   const [name, setName] = useState('');
   const [sum, setSum] = useState('');
   const [date, setDate] = useState('');
 
+  // const [userInput, setUserInput] = useState({
+  //   name: '',
+  //   sum: '',
+  //   date: ''
+  // });
+
   const nameChangeHandler = (event) => {
     setName(event.target.value);
+  //   setUserInput({
+  //     ...userInput,
+  //     name: event.target.value
+  //   })    
   }
   const sumChangeHandler = (event) => {
     setSum(event.target.value)  
+    // setUserInput({
+    //   ...userInput,
+    //   sum: event.target.value
+    // })
   }
   
   const dateChangeHandler = (event) => {
     setDate(event.target.value)  
+
+    // setUserInput({
+    //   ...userInput,
+    //   date: event.target.value
+    // })
+
+    // setUserInput((previousState) => {
+    //   return {
+    //     ...previousState,
+    //     name: event.target.value
+    //   }
+    // })
   }
 
+  const submitHandler = (event) => {
+    event.preventDefault();
+
+    const costData = {
+      name: name,
+      sum: sum,
+      date: new Date(date)
+    };
+
+    props.onSaveCostData(costData);
+    setName('')
+    setSum('')
+    setDate('')
+
+  };
+
   return (
-    <form>
+    <form onSubmit={submitHandler}>
       <div className="new-cost__controls">
         <div className="new-cost__control">
           <label>Название</label>
-          <input type="text" onChange={nameChangeHandler}/>
+          <input type="text" value={name} onChange={nameChangeHandler}/>
         </div>
         <div className="new-cost__control">
           <label>Сумма</label>
-          <input type="number" min='0.01' step='0.01' onChange={sumChangeHandler}/>
+          <input type="number" min='0.01' step='0.01' value={sum} onChange={sumChangeHandler}/>
         </div>
         <div className="new-cost__control">
           <label>Дата</label>
-          <input type="date" min='2019-01-01' onChange={dateChangeHandler}/>
+          <input type="date" min='2019-01-01' value={date} onChange={dateChangeHandler}/>
         </div>
         <div className="new-cost__actions">
           <button type="submit">Добавить расход</button>
